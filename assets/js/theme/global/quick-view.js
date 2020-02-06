b0e8ff163c08e98baec73cc535c5bc80e7bd625a
Kyle <obrienk@webbhost.net>
2020-02-06 16:31:37 -0600

    add multi-product-view t to display PDP within multi-view.html retrieved via stencil utils, combined with JS based iteration, this now works

--- assets/js/theme/global/quick-view.js
+++ assets/js/theme/global/quick-view.js
@@ -7,23 +7,7 @@ import 'slick-carousel';
 
 export default function (context) {
     const modal = defaultModal();
-    console.log(context)
-    if (context['productIds']) {
-        
 
-        console.log([context['productIds']]);
-        [context['productIds']].forEach((id) => {
-            utils.api.product.getById(id, { template: 'products/quick-view' }, (err, response) => {
-                let sel = document.querySelector('#multi-desc');
-                console.log(sel + '18');
-                sel.innerHTML = response;
-                console.log(sel + '20');
-                console.log(err);
-            });
-        })
-    
-        
-    }
     $('body').on('click', '.quickview', event => {
         event.preventDefault();
 
